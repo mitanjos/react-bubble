6f9babd5cae45e28457c0003081d389ffa40b980
Keyur joshi <joshikeyur@gmail.com>
2018-09-16 10:17:28 +0530

    final copy

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export const calculateCicleDimensions = (percentage) => {
 	var denom3 = calculateDemon(usage[2])
 	var maxDenom = Math.max(Math.max(denom1,denom2),denom3)
 	console.log("calculateCicleDimensions::denom",maxDenom)
-	var scalingFactor = 95 / maxDenom
+	var scalingFactor = 100 / maxDenom
 	console.log("Actions::calculateCicleDimensions::scalingFactor",scalingFactor)
 	var circle1 = calculateCicleLocationAndRadius(usage[0],scalingFactor)
 	var circle2 = calculateCicleLocationAndRadius(usage[1],scalingFactor)
@@ -42,35 +42,44 @@ const calculateDemon = (data) => {
 const calculateCicleLocationAndRadius = (data,scalingFactor) => {
 	console.log("calculateCicleLocationAndRadius::",data,scalingFactor)
 	var retArray = []
+
+	var r1 = (data[0]*scalingFactor/2)
+	var r2 = (data[1]*scalingFactor/2)
+	var r3 = (data[2]*scalingFactor/2)
+	var r4 = (data[3]*scalingFactor/2)
+	var r5 = (data[4]*scalingFactor/2)
 	
-	var cx1 = (data[0]*scalingFactor/2)+"%"
-	var cy1 = (data[0]*scalingFactor/2)+"%"
-	var r1 = (data[0]*scalingFactor/2)+"%"
-	var circle1 = {x:cx1,y:cy1,r:r1}
+	var marginY1 = (100 - (r1+r3)*2)/4
+	var marginY2 = (100 - (r5+r2)*2)/4
+	var marginX1 = (100 - (r1+r2)*2)/4
+	var marginX2 = (100 - (r3+r4+r5)*2)/6
+
+	console.log("Margin y1",r1,r3,marginY1)
+	var cx1 = r1+marginX1
+	var cy1 = r1+marginY1
+	
+	var circle1 = {x:cx1+"%",y:cy1+"%",r:r1+"%"}
 	retArray.push(circle1)
 
-	var cx2 = (100 - (data[1]*scalingFactor/2))+"%"
-	var cy2 = (data[1]*scalingFactor/2)+"%"
-	var r2 = (data[1]*scalingFactor/2)+"%"
-	var circle2 = {x:cx2,y:cy2,r:r2}
+	var cx2 = 100 - r2 -marginX1
+	var cy2 = r2+marginY2
+	var circle2 = {x:cx2+"%",y:cy2+"%",r:r2+"%"}
 	retArray.push(circle2)
 
-	var cx3 = (data[2]*scalingFactor/2)+"%"
-	var cy3 = (100-(data[2]*scalingFactor/2))+"%"
-	var r3 = (data[2]*scalingFactor/2)+"%"
-	var circle3 = {x:cx3,y:cy3,r:r3}
+	var cx3 = r3 + marginX2
+	var cy3 = 100-r3-marginY1
+	var circle3 = {x:cx3+"%",y:cy3+"%",r:r3+"%"}
 	retArray.push(circle3)
 
-	var cx5 = (100-(data[4]*scalingFactor/2))+"%"
-	var cy5 = (100-(data[4]*scalingFactor/2))+"%"
-	var r5 = (data[4]*scalingFactor/2)+"%"
-	var circle5 = {x:cx5,y:cy5,r:r5}
+	var cx5 = 100-r5-marginX2
+	var cy5 = 100-r5-marginY2
+	var circle5 = {x:cx5+"%",y:cy5+"%",r:r5+"%"}
 	retArray.push(circle5)
 
-	var cx4 = "50%"
-	var cy4 = (100-(data[3]*scalingFactor/2))+"%"
-	var r4 = (data[3]*scalingFactor/2)+"%"
-	var circle4 = {x:cx4,y:cy4,r:r4}
+	var cx4 = ((r3+marginX2)*2)+r4+marginX2
+	var cy4 = (cy3+cy5)/2
+	console.log("==>",cx4,cy3,cy5,(cy3+cy5)/2)
+	var circle4 = {x:cx4+"%",y:cy4+"%",r:r4+"%"}
 	retArray.push(circle4)
 
 	return retArray
